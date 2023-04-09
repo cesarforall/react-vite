ef2ac22443ddf9e971c97103768c7ac2c2491d90
César Almeida <cesaralmeida89@gmail.com>
2023-04-09 17:21:07 +0200

    :hammer: add isFollowing prop to TwitterFollowCard

--- projects/hello-world/src/App.jsx
+++ projects/hello-world/src/App.jsx
@@ -4,8 +4,8 @@ import { TwitterFollowCard } from './components/TwitterFollowCard';
 export function App() {
 	return (
 		<div style={{ width: '320px' }}>
-			<TwitterFollowCard name='César Almeida Reyes' userName='cesarforall' />
-			<TwitterFollowCard name='Miguel Ángel Durán' userName='midudev' />
+			<TwitterFollowCard isFollowing name='César Almeida Reyes' userName='cesarforall' />
+			<TwitterFollowCard isFollowing={false} name='Miguel Ángel Durán' userName='midudev' />
 		</div>
 	);
 }
--- projects/hello-world/src/components/TwitterFollowCard.jsx
+++ projects/hello-world/src/components/TwitterFollowCard.jsx
@@ -1,10 +1,11 @@
-export function TwitterFollowCard({ name, userName }) {
+export function TwitterFollowCard({ name, userName, isFollowing }) {
+	console.log(`${name} is following: ${isFollowing}`);
 	const imgSrc = `https://unavatar.io/${userName}`;
 	return (
 		<article className='tw-followCard'>
-			<header className="tw-followCard__header">
-				<img className="tw-followCard__avatar" src={imgSrc} alt={`El avatar de ${userName}`} />
-				<div className="tw-followCard-info">
+			<header className='tw-followCard__header'>
+				<img className='tw-followCard__avatar' src={imgSrc} alt={`El avatar de ${userName}`} />
+				<div className='tw-followCard-info'>
 					<strong>{name}</strong>
 					<span>@{userName}</span>
 				</div>

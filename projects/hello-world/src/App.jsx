892c791e76fa0bd457951bbc66157c26f15a7a67
César Almeida <cesaralmeida89@gmail.com>
2023-04-11 19:12:05 +0200

    :lipstick: add cardsContainer styles

--- projects/hello-world/src/App.jsx
+++ projects/hello-world/src/App.jsx
@@ -3,7 +3,7 @@ import { TwitterFollowCard } from './components/TwitterFollowCard';
 
 export function App() {
 	return (
-		<div style={{ width: '320px' }}>
+		<div className='cardsContainer'>
 			<TwitterFollowCard isFollowing name='César Almeida Reyes' userName='cesarforall' />
 			<TwitterFollowCard isFollowing={false} name='Miguel Ángel Durán' userName='midudev' />
 		</div>
